39e899a669c22f304df80cff42686a0f656ac4d9
Juyeon Lee <lqu134@naver.com>
2024-05-05 00:12:20 +0900

    Todo UI completed

---
+++ ToDo/src/components/Editor.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Editor.css";
+
+export default function Editor() {
+  return (
+    <div className="Editor">
+      <input placeholder="새로운 Todo..." />
+      <button>추가</button>
+    </div>
+  );
+}
---
+++ ToDo/src/components/TodoItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./TodoItem.css";
+
+export default function TodoItem() {
+  return (
+    <div className="TodoItem">
+      <input type="checkbox" />
+      <div className="content">Todo...</div>
+      <div className="date">Date</div>
+      <button>삭제</button>
+    </div>
+  );
+}
