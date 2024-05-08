958cee0a714de7bfbe003fd1d4918f73c329178e
Juyeon Lee <lqu134@naver.com>
2024-05-08 23:32:11 +0900

    rendering todolist

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -2,13 +2,49 @@ import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import { useRef, useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false, 
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false, 
+    content: "JS 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false, 
+    content: "Yuuka 공부하기",
+    date: new Date().getTime(),
+  },
+]
+
 
 function App() {
+  const [todos, setTodos] = useState(mockData)
+  const idRef = useRef(3)
+ 
+
+  const onCreate = (create) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    }
+    setTodos([newTodo,...todos])
+  }
   return (
     <div className="App">
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} />
     </div>
   );
 }
--- ToDo/src/components/Editor.jsx
+++ ToDo/src/components/Editor.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Editor.css";
 
-export default function Editor() {
+export default function Editor({onCreate}) {
+  const [contents, setContents] = useState("")
+  const contentRef = useRef()
+
+  const onChangeContent = (e) => {
+    setContents(e.target.value)
+  }
+
+  const onKeyDown = (e) => {
+    if(e.keyCode === 13){
+      onSubmit()
+    }
+  }
+
+  const onSubmit = () => {
+    if(contents === ""){
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(contents);
+    setContents("")
+  }
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input ref={contentRef} value={contents} onKeyDown={onKeyDown}onChange={onChangeContent}placeholder="새로운 Todo..." />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 }
